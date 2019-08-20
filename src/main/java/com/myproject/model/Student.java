6020b2849de51475358c9fa05d37dd21c2a80914
Priyanka Sarkar <priyasar@amazon.com>
2019-08-20 22:24:40 +0530

    Student model to define the attributes and behaviour of a Student

---
+++ src/main/java/com/myproject/model/Student.java
@@ -0,0 +1,44 @@
+package com.myproject.model;
+
+public class Student {
+	
+	private Integer rollNo;
+	private String firstName;
+	private String lastName;
+	private String department;
+	
+	public Integer getRollNo() {
+		return rollNo;
+	}
+	public void setRollNo(Integer rollNo) {
+		this.rollNo = rollNo;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	
+	@Override
+	public String toString() {
+		return "Student [rollNo=" + rollNo + ", firstName=" + firstName + ", lastName=" + lastName + ", department="
+				+ department + "]";
+	}
+	
+}
